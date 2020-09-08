7263b90f4ee21003768f91aa7179d75ae732960d
daniela-ops <66274413+daniela-ops@users.noreply.github.com>
2020-09-08 14:28:02 -0400

    Added GitHub repo URL

--- src/App.js
+++ src/App.js
@@ -2,7 +2,20 @@ import React from "react";
 import "./App.css";
 
 function App() {
-  return <div className="App">Hello World!</div>;
+  return (
+    <div className="App">
+      Hello World!
+      <footer>
+        This project was coded by Daniela Arias and is{" "}
+        <a
+          href="https://github.com/wecodeschool/react-weather-app.git"
+          target="_blank"
+        >
+          open-sourced on GitHub{" "}
+        </a>
+      </footer>
+    </div>
+  );
 }
 
 export default App;
