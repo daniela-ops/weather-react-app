017e2aa25fc6cfeec21d614b93bed4bfcd410faf
daniela-ops <66274413+daniela-ops@users.noreply.github.com>
2020-09-08 15:11:45 -0400

    Added BootStrap

--- src/App.js
+++ src/App.js
@@ -4,16 +4,18 @@ import "./App.css";
 function App() {
   return (
     <div className="App">
-      Hello World!
-      <footer>
-        This project was coded by Daniela Arias and is{" "}
-        <a
-          href="https://github.com/wecodeschool/react-weather-app.git"
-          target="_blank"
-        >
-          open-sourced on GitHub{" "}
-        </a>
-      </footer>
+      <div className="container">
+        Hello World!
+        <footer>
+          This project was coded by Daniela Arias and is{" "}
+          <a
+            href="https://github.com/wecodeschool/react-weather-app.git"
+            target="_blank"
+          >
+            open-sourced on GitHub{" "}
+          </a>
+        </footer>
+      </div>
     </div>
   );
 }
